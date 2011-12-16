596601dab7e5215ad13fc44eedf3e50ddc31ece5
Robert Levy <r.p.levy@gmail.com>
2011-12-15 20:36:30 -0500

    add twitter-api dependency

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject oinque "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.expressme/JOpenId "1.08"]]
+                 [twitter-api "0.6.4"]]
   :dev-dependencies [[midje "1.3.0"]]
   :main oinque.core)

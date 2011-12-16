8662ddab0dc4e18f74055778c03d736386e9b24c
Robert Levy <r.p.levy@gmail.com>
2011-12-15 20:31:43 -0500

    add some dependencies

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject oinque "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.expressme/JOpenId "1.08"]]
+  :dev-dependencies [[midje "1.3.0"]]
+  :main oinque.core)
